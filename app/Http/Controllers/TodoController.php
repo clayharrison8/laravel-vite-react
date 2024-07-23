d71848661841b50d197bf5329c435ae66f3d084b
clayharrison8 <86426561+clayharrison8@users.noreply.github.com>
2024-07-23 18:08:13 +0100

    added api/controller for crud operations

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -3,62 +3,38 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Todo;
 
 class TodoController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
     public function index()
     {
-        //
+        return response()->json(Todo::all());
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'text' => 'required|string',
+            'completed' => 'required|boolean',
+        ]);
+
+        $todo = Todo::create($request->all());
+
+        return response()->json($todo, 201);
     }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
+    public function update(Request $request, $id)
     {
-        //
+        $todo = Todo::findOrFail($id)->update($request->all());
+
+        return response()->json($todo);
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
+    public function destroy($id)
     {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        //
+        Todo::findOrFail($id)->delete();
+        
+        return response()->json(null, 204);
     }
 }
---
+++ routes/api.php
@@ -0,0 +1,8 @@
+<?php
+
+use App\Http\Controllers\TodoController;
+
+Route::get('/todos', [TodoController::class, 'index']);
+Route::post('/todos', [TodoController::class, 'store']);
+Route::put('/todos/{id}', [TodoController::class, 'update']);
+Route::delete('/todos/{id}', [TodoController::class, 'destroy']);
